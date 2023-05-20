96c26baabb9cbfa298d366162fe958d271333c4e
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-20 20:59:30 +0300

    Update css

--- RestaurantManagement/src/main/java/com/example/RestaurantManagement/Controllers/OrderController.java
+++ RestaurantManagement/src/main/java/com/example/RestaurantManagement/Controllers/OrderController.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class OrderController {
@@ -47,15 +47,9 @@ public class OrderController {
     public String submitOrder(@RequestParam Integer table_id,
                               @RequestParam List<Integer> dish_ids,
                               RedirectAttributes redirectAttributes) {
-        // Create the new order and save it to the database
         Order order = new Order();
-        // Here you should fetch the Table entity from database using the provided table_id
-        // For this you will need a TableRepository
-        // order.setTable(table);
         order.setTotalCost(calculateTotalCost(dish_ids));
         orderRepository.save(order);
-
-        // Add the ordered dishes to the OrderedDish table
         for (Integer dish_id : dish_ids) {
             Optional<Dish> dishOptional = dishRepository.findById(dish_id);
             if (dishOptional.isPresent()) {
@@ -66,9 +60,8 @@ public class OrderController {
             }
         }
 
-        // Redirect to a confirmation page or wherever you want
-        redirectAttributes.addFlashAttribute("message", "Order created successfully!");
-        return "redirect:/confirmation";
+        redirectAttributes.addFlashAttribute("message", "Заказ успешно создан!");
+        return "redirect:/view-orders";
     }
 
     private double calculateTotalCost(List<Integer> dish_ids) {
--- RestaurantManagement/src/main/java/com/example/RestaurantManagement/Repositories/OrderedDishRepository.java
+++ RestaurantManagement/src/main/java/com/example/RestaurantManagement/Repositories/OrderedDishRepository.java
@@ -1,10 +1,14 @@
 package com.example.RestaurantManagement.Repositories;
 
+import com.example.RestaurantManagement.Models.Order;
 import com.example.RestaurantManagement.Models.OrderedDish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderedDishRepository extends JpaRepository<OrderedDish, Integer> {
+    List<OrderedDish> findByOrder(Order order);
 }
 
